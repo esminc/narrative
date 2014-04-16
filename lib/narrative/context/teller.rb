67d42bd46871bf7d5e25360a31e27ba0bde38dcd
kajisha <h-kajisha@esm.co.jp>
2014-04-16 18:10:34 +0900

    add test for with_context

--- lib/narrative/context/teller.rb
+++ lib/narrative/context/teller.rb
@@ -1,14 +1,17 @@
+require 'active_support/core_ext/string/inflections'
+
 module Narrative
   module Context
     module Teller
       def with_context(context_name, data, &block)
-        context_for(context_name).bind! data, block
+        context_for(context_name).bind! data, &block
       end
 
       private
 
       def context_for(name)
-        name.split.map(&:capitalize).join.constantize
+        context_names = name.split << 'context'
+        context_names.map(&:capitalize).join.constantize
       end
     end
   end
