db884d5b86e3bd775a7d126390da381e4710e1c4
kajisha <h-kajisha@esm.co.jp>
2014-04-16 14:56:11 +0900

    casting roles

--- lib/narrative/context.rb
+++ lib/narrative/context.rb
@@ -11,6 +11,24 @@ module Narrative
         self.roles ||= {}
         roles[name] = block
       end
+
+      def self.bind!(data, &block)
+        block.call bind_roles!(data, &block)
+      end
+
+      private
+
+      def self.bind_roles!(data, &block)
+        context_roles = block.parameters.map(&:last)
+        context_roles.each_with_object({}) {|role_name, actors|
+          actors[role_name] = cast!(data[role_name], &roles[role_name])
+        }
+      end
+
+      def self.cast!(datum, &method_block)
+        datum.instance_eval(&method_block)
+        datum
+      end
     end
   end
 end
--- spec/lib/narrative/context_spec.rb
+++ spec/lib/narrative/context_spec.rb
@@ -5,20 +5,37 @@ describe Narrative::Context do
     class ProjectContext
       include Narrative::Context
 
-      role :programmer do
-        def coding; end
-      end
+      role :programmer do; end
     end
 
     class AnotherProjectContext
       include Narrative::Context
 
-      role :product_owner do
-        def accept_story; end
-      end
+      role :product_owner do; end
     end
 
     it { expect(ProjectContext.roles).to include(:programmer) }
     it { expect(ProjectContext.roles).not_to include(:product_owner) }
+  end
+
+  describe '.bind' do
+    class ProjectContext
+      include Narrative::Context
+
+      role :programmer do
+        def coding; end
+      end
+    end
+
+    specify do
+      called = false
+      ProjectContext.bind! programmer: 'alice' do |programmer:|
+        expect(programmer).to respond_to(:coding)
+
+        called = true
+      end
+
+      expect(called).to eq(true)
+    end
   end
 end
