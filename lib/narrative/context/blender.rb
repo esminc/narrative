6b7ba6ed0a90c2a27346065c374cd52d1fbed2bd
kajisha <h-kajisha@esm.co.jp>
2014-04-16 15:23:14 +0900

    use bind!

--- lib/narrative/context/blender.rb
+++ lib/narrative/context/blender.rb
@@ -2,8 +2,7 @@ module Narrative
   module Context
     module Blender
       def with_context(context_name, attributes, &block)
-        context = context_for(context_name).new(attributes)
-        context.bind block
+        context_for(context_name).bind! attributes, block
       end
 
       private
