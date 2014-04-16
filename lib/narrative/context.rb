8101ccfc821649bb420d528dda4e962d165bc082
kajisha <h-kajisha@esm.co.jp>
2014-04-16 15:57:55 +0900

    extract ClassMethods

--- lib/narrative/context.rb
+++ lib/narrative/context.rb
@@ -7,25 +7,28 @@ module Narrative
     included do
       cattr_accessor :roles, instance_accessor: false
 
-      def self.role(name, &block)
-        self.roles ||= {}
+      self.roles ||= {}
+    end
+
+    module ClassMethods
+      def role(name, &block)
         roles[name] = block
       end
 
-      def self.bind!(data, &block)
+      def bind!(data, &block)
         block.call bind_roles!(data, &block)
       end
 
       private
 
-      def self.bind_roles!(data, &block)
+      def bind_roles!(data, &block)
         context_roles = block.parameters.map(&:last)
         context_roles.each_with_object({}) {|role_name, actors|
           actors[role_name] = cast!(data[role_name], &roles[role_name])
         }
       end
 
-      def self.cast!(datum, &method_block)
+      def cast!(datum, &method_block)
         datum.instance_eval(&method_block)
         datum
       end
